eff80087ba335e4f8de5dc22af8d9088afeb8aa8
Max <48124450+maxpertici@users.noreply.github.com>
2020-08-16 18:47:38 +0200

    lazy - init project

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package LAZY
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lazy' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'lazy' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'lazy' ), 'LAZY', '<a href="https://m.pertici.fr">maxpertici</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+
+// requires
+var gulp        = require("gulp");
+var sass        = require("gulp-sass");
+var browserSync = require("browser-sync").create();
+
+var autoprefixer = require("gulp-autoprefixer");
+var plumber      = require("gulp-plumber");
+var sourcemaps   = require('gulp-sourcemaps');
+
+
+function lazy_sass_task(){
+
+    return (
+        gulp
+        .src( ['./sass/*.scss','./sass/**/*.scss'] )
+        .pipe( sass().on('error', sass.logError) )
+        .pipe( gulp.dest('./css/') )
+        .pipe( browserSync.stream() )
+    );
+
+}
+
+function lazy_js_task(){
+
+    return (
+        gulp.task('js', function () {
+            return gulp.src('./js/*js')
+                // .pipe( browserify() )
+                // .pipe( uglify() )
+                .pipe(gulp.dest('./js/dist'));
+        })
+    );
+}
+
+function lazy_js_reload(){
+    browserSync.reload();
+    done();
+}
+
+
+function lazy_launch(){
+    
+    browserSync.init({
+      proxy: "https://mamp-sites.dev/lazy/",
+      // ghostMode: false,
+      // open: false,
+      // notify: false
+    })
+
+    // sass
+    gulp.watch("./sass/*.scss", lazy_sass_task );
+    gulp.watch("./sass/**/*.scss", lazy_sass_task );
+    gulp.watch("./sass/**/**/*.scss", lazy_sass_task );
+    
+    // js
+    // gulp.watch("./js/*.js", lazy_js_task );
+    gulp.watch("./js/*.js", lazy_js_reload );
+
+    // php
+    // gulp.watch("./*.php"   ).on('change', browserSync.reload);
+    // gulp.watch("./**/*.php").on('change', browserSync.reload);
+
+}
+
+gulp.task('default', lazy_launch );
